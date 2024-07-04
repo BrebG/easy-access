67720e9169e9cd448d5752b047632e29e2693d47
ileanaspl <ileana.saintpaul@gmail.com>
2024-07-04 16:53:52 +0200

    before pull

--- src/car.js
+++ src/car.js
@@ -6,16 +6,42 @@ loadSprite("car", "sprites/voiture1.png");
 loadSprite("car2", "sprites/voiture2.png");
 loadSprite("player", "sprites/player.png");
 loadSprite("metal", "sprites/metal.png");
+loadSprite("pedestrian", "sprites/bean.png");
+loadSprite("grass", "sprites/grass.png");
+loadSprite("road", "sprites/road.jpg");
 
 let SPEED = 480;
 
 const carSpeed = 200;
 const carDirection = vec2(1, 0);
 
+function createPedestrian(x, y) {
+	const pedestrianSpeed = 100;
+	const pedestrianDirection = choose([
+		vec2(1, 0),
+		vec2(-1, 0),
+		vec2(0, 1),
+		vec2(0, -1),
+	]);
+
+	const pedestrian = add([
+		sprite("pedestrian"),
+		pos(x, y),
+		area(),
+		body({ isStatic: true }),
+		move(pedestrianDirection, pedestrianSpeed),
+		"pedestrian",
+	]);
+
+	wait(10, () => {
+		destroy(pedestrian);
+	});
+}
+
 function spawnFirstCar() {
 	add([
 		sprite("car"),
-		pos(0, 250),
+		pos(0, 280),
 		area(),
 		body(),
 		move(carDirection, carSpeed),
@@ -26,7 +52,7 @@ function spawnFirstCar() {
 function spawnSecondCar() {
 	add([
 		sprite("car2"),
-		pos(1920, 400),
+		pos(1920, 380),
 		area(),
 		body(),
 		move(vec2(-1, 0), carSpeed),
@@ -37,7 +63,7 @@ function spawnSecondCar() {
 function spawnThirdCar() {
 	add([
 		sprite("car"),
-		pos(0, 550),
+		pos(0, 480),
 		area(),
 		body(),
 		move(carDirection, carSpeed),
@@ -48,7 +74,7 @@ function spawnThirdCar() {
 function spawnFourthCar() {
 	add([
 		sprite("car"),
-		pos(0, 700),
+		pos(0, 580),
 		area(),
 		body(),
 		move(carDirection, carSpeed),
@@ -57,48 +83,88 @@ function spawnFourthCar() {
 	wait(rand(1, 4), spawnFourthCar);
 }
 
-export default carScene = () => {
-	scene("car", () => {
-		spawnFirstCar();
-		spawnSecondCar();
-		spawnThirdCar();
-		spawnFourthCar();
+scene("car", () => {
+	add([sprite("road", { width: width("100vw"), height: height("100vh") })]);
 
-		const player = add([sprite("player"), pos(80, 40), area(), "player"]);
-		const metal = add([
-			sprite("metal"),
-			pos(1700, 800),
-			area(),
-			body(),
-			"metal",
-		]);
+	spawnFirstCar();
+	spawnSecondCar();
+	spawnThirdCar();
+	spawnFourthCar();
 
-		player.onCollide("metal", () => {
-			go("maze");
-		});
+	const player = add([sprite("player"), pos(80, 40), area(), "player"]);
+	const metal = add([sprite("metal"), pos(1700, 800), area(), body(), "metal"]);
 
-		player.onCollide("car", () => {
-			burp();
-			addKaboom(player.pos);
-			go("car");
-		});
-
-		onKeyDown("left", () => {
-			player.move(-SPEED, 0);
-		});
-
-		onKeyDown("right", () => {
-			player.move(SPEED, 0);
-		});
-
-		onKeyDown("up", () => {
-			player.move(0, -SPEED);
-		});
-
-		onKeyDown("down", () => {
-			player.move(0, SPEED);
-		});
+	player.onCollide("metal", () => {
+		go("maze");
 	});
-	go("car");
-};
+
+	player.onCollide("car", () => {
+		burp();
+		addKaboom(player.pos);
+		go("car");
+	});
+
+	onKeyDown("left", () => {
+		player.move(-SPEED, 0);
+	});
+
+	onKeyDown("right", () => {
+		player.move(SPEED, 0);
+	});
+
+	onKeyDown("up", () => {
+		player.move(0, -SPEED);
+	});
+
+	onKeyDown("down", () => {
+		player.move(0, SPEED);
+	});
+});
+
+scene("maze", () => {
+	add([sprite("grass", { width: width("100vw"), height: height("100vh") })]);
+
+	loop(1, () => {
+		createPedestrian(rand(0, width()), rand(0, height()));
+	});
+
+	const player = add([sprite("player"), pos(70, 800), area(), "player"]);
+	const metal = add([
+		sprite("metal"),
+		pos(1750, 90),
+		area(),
+		body({ isStatic: true }),
+		"metal",
+	]);
+
+	player.onCollideUpdate("pedestrian", () => {
+		SPEED = 200;
+	});
+
+	player.onCollideEnd("pedestrian", () => {
+		SPEED = 480;
+	});
+
+	player.onCollide("metal", () => {
+		go("car");
+	});
+
+	onKeyDown("left", () => {
+		player.move(-SPEED, 0);
+	});
+
+	onKeyDown("right", () => {
+		player.move(SPEED, 0);
+	});
+
+	onKeyDown("up", () => {
+		player.move(0, -SPEED);
+	});
+
+	onKeyDown("down", () => {
+		player.move(0, SPEED);
+	});
+});
+
+go("car");
 carScene();
