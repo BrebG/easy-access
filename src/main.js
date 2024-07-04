5167c9e581c30237a976b2f32731165120583fa1
BrebG <brebelgael@protonmail.com>
2024-07-04 11:04:45 +0200

    fix: commit prepull

--- src/main.js
+++ src/main.js
@@ -1,32 +1,148 @@
 import kaboom from "kaboom"
 
+// Initialize Kaboom
 kaboom()
 
+// Constants
 const SPEED = 320
 
-loadSprite("bean", "/sprites/bean.png")
+// Load assets
+loadSprite("bean", "/sprites/bean.png");
+loadSprite("light_switch", "https://kaboomjs.com/sprites/coin.png");
 
-scene("game", () => {
-	const player = add([
-		sprite("bean"),
-		pos(100, 200),
-		area(),
-		body(),
-		"player",
+// Game state
+let isLightOn = true;
+let canToggleLight = false;
+let canTakeElevator = false;
+let blackScreen = null;
+
+function addDialog() {
+	const h = 160
+	const pad = 16
+	const bg = add([
+		pos(0, height() - h),
+		rect(width(), h),
+		color(0, 0, 0),
+		z(100),
 	])
-
-	const dirs = {
-		"left": LEFT,
-		"right": RIGHT,
-		"up": UP,
-		"down": DOWN,
+	const txt = add([
+		text("", {
+			width: width(),
+		}),
+		pos(0 + pad, height() - h + pad),
+		z(100),
+	])
+	bg.hidden = true
+	txt.hidden = true
+	return {
+		say(t) {
+			txt.text = t
+			bg.hidden = false
+			txt.hidden = false
+		},
+		dismiss() {
+			if (!this.active()) {
+				return
+			}
+			txt.text = ""
+			bg.hidden = true
+			txt.hidden = true
+		},
+		active() {
+			return !bg.hidden
+		},
+		destroy() {
+			bg.destroy()
+			txt.destroy()
+		},
 	}
+}
+const dirs = {
+	"left": LEFT,
+	"right": RIGHT,
+	"up": UP,
+	"down": DOWN,
+}
 
-	for (const dir in dirs) {
-		onKeyDown(dir, () => {
-			player.move(dirs[dir].scale(SPEED))
-		})
-	}
+// Player
+const player = add([
+	sprite("bean"),
+	pos(100, 200),
+	area(),
+	body(),
+	"player",
+])
+
+// Light switch
+const lightSwitch = add([
+	sprite("light_switch"),
+	pos(300, 100),
+	area(),
+	"light_switch"
+]);
+
+// Dialog
+const dialog = addDialog()
+
+// Player movement
+for (const dir in dirs) {
+	onKeyPress(dir, () => {
+		dialog.dismiss()
+	})
+	onKeyDown(dir, () => {
+		player.move(dirs[dir].scale(SPEED))
+	})
+}
+
+// Player collisions
+player.onCollide("elevatorOOS", () => {
+	dialog.say("The elevator is out of service, I should go find an other one.");
 })
 
-go("game")
+player.onCollide("elevator", () => {
+	dialog.say("Would you like to use the elevator ? Press 'E' to accept");
+	canTakeElevator = true;
+	onKeyPress("e", () => {
+		if (canTakeElevator) {
+			go("next_level", levelIdx + 1)
+		}
+	})
+})
+
+player.onCollideEnd("elevator", () => {
+	canTakeElevator = false;
+})
+
+player.onCollide("light_switch", () => {
+	dialog.say("You found a light switch ! Press 'E' to activate");
+	canToggleLight = true;
+})
+
+player.onCollideEnd("light_switch", () => {
+	canToggleLight = false;
+})
+
+player.onCollide("bus", () => {
+	dialog.say("Sorry Miss, the access ramp is out of service. The next bus should be here in 20 minutes.");
+})
+
+// Toggle light
+onKeyPress("e", () => {
+	if (canToggleLight) {
+		if (isLightOn) {
+			isLightOn = false;
+			blackScreen = add([
+				rect(width(), height()),
+				pos(0, 0),
+				color(0, 0, 0),
+				"black_screen"
+			]);
+		} else {
+			isLightOn = true;
+			if (blackScreen) {
+				destroy(blackScreen);
+				blackScreen = null;
+			}
+		}
+	}
+});
